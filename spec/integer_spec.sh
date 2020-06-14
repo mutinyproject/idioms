2b0774cb40fe7b1ed3f80eabb3e8868d78ea148b
Kylie McClain <kylie@somas.is>
2020-06-14 04:42:06 -0400

    integer: use non-negative rather than positive
    Signed-off-by: my wife

--- spec/integer_spec.sh
+++ spec/integer_spec.sh
@@ -1,5 +1,5 @@
 Describe "integer"
-    It "succeeds when given a positive integer"
+    It "succeeds when given a non-negative integer"
         When call integer 1
         The status should equal 0
     End
@@ -14,17 +14,17 @@ Describe "integer"
         The status should equal 1
     End
 
-    It "succeeds when multiple arguments are given, all of them integers"
+    It "succeeds when multiple arguments are given, all of them a non-negative integers"
         When call integer 1 2 3
         The status should equal 0
     End
 
-    It "fails when multiple arguments are given, none of them integers"
+    It "fails when multiple arguments are given, none of them a non-negative integers"
         When call integer one two three
         The status should equal 1
     End
 
-    It "fails when multiple arguments are given, only one of them being integers"
+    It "fails when multiple arguments are given, only one of them being a non-negative integers"
         When call integer 1 two three
         The status should equal 1
     End
