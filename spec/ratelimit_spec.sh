5ae56470188a365550a2ff8592bc3fbee4be6f6c
Kylie McClain <kylie@somas.is>
2020-07-13 01:15:10 -0400

    ratelimit: make -e *add* strings to result, not overwrite generated name

--- spec/ratelimit_spec.sh
+++ spec/ratelimit_spec.sh
@@ -1,7 +1,7 @@
 XDG_RUNTIME_DIR="${TMPDIR:-/tmp}"
 
 print_temp() {
-    ratelimit ${@:+"$@"} 3 true &
+    ratelimit ${@:+"$@"} 2 true &
     (sleep 1 && echo "${XDG_RUNTIME_DIR}"/ratelimit/*.lock)
 }
 
@@ -9,5 +9,10 @@ Describe "ratelimit"
     It "runs a command, limiting the rate that it can be ran"
         When run print_temp
         The stdout should equal "${XDG_RUNTIME_DIR}/ratelimit/2469621148.lock"
+    End
+
+    It "allows for adding entropy to the lock file's name"
+        When run print_temp -e "entropy"
+        The stdout should equal "${XDG_RUNTIME_DIR}/ratelimit/4069469580.lock"
     End
 End
